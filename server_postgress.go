0910c64a2e09283b0f356c394ffc7836d17b03b2
AndriyStasyuk <36013142+AndriyStasyuk@users.noreply.github.com>
2018-04-28 21:21:06 +0300

    Update server_postgress.go

--- server_postgress.go
+++ server_postgress.go
@@ -1,112 +1,90 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	"os"
-	"log"
-	"net/http"
 	"encoding/json"
-	"github.com/jinzhu/gorm"
 	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"os"
+	"github.com/jinzhu/gorm"
+	"time"
 )
 
-type Product struct {
-	gorm.Model
-    Id         int    `sql:"id"`
-    Key        string `sql:"key"`
-    FirstName  string `sql:"first_name"`
-    SecondName string `sql:"second_name"`
-    Status     string `sql:"status"`
+type Log struct	{
+	Id			int64		`sql:"id"`
+	UserId		int64		`sql:"user_id"`
+	CreatedAt	time.Time	`sql:"created_at"`
+	EventType	string		`sql:"event_type"`
 }
 
-var db	*gorm.DB
-var err	error
+type User struct {
+
+	Id              int         `sql:"id"`
+	CardKey         int64       `sql:"card_key"`
+	FirstName       string      `sql:"first_name"`
+	LastName        string      `sql:"last_name"`
+	Status          string      `sql:"status"`
+	LastCheckedIn   time.Time	 `sql:"last_checked_in"`
+}
+
+var db  *gorm.DB
+
 
 func main() {
-	router := mux.NewRouter()
 
 	host := os.Getenv("HOST")
 	user := os.Getenv("USER")
 	password := os.Getenv("PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 
-	connStr := fmt.Sprintf("host=%v user=%v dbname=%v sslmode=enable password=%v", host, user, dbName, password)
-
-	db, err := gorm.Open("postgres", connStr)
-
+	connStr := fmt.Sprintf("host=%v user=%v dbname=%v sslmode=require password=%v", host, user, dbName, password)
+	ddb, err := gorm.Open("postgres", connStr)
+	db = ddb
 	if err != nil {
 		panic("Failed to connect database")
 	}
+	ddb.LogMode(true)
 
-	defer db.Close()
+	defer ddb.Close()
+
+	router := mux.NewRouter()
 
 	router.HandleFunc("/std", GetResources).Methods("GET")
-	router.HandleFunc("/std/{key}", GetResource).Methods("GET")
-	router.HandleFunc("/std", CreateResource).Methods("POST")
-	router.HandleFunc("/std/{key}", DeleteResource).Methods("DELETE")
+	router.HandleFunc("/std/user", GetResource).Methods("POST")
+	router.HandleFunc("/std/create", CreateResource).Methods("POST")
+	router.HandleFunc("/std/delete/{card_key}", DeleteResource).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+	http.ListenAndServe("localhost:8000", router)
+
 }
 
 func GetResources(w http.ResponseWriter, r *http.Request) {
-	var resources []Product
-
-	if err := db.Find(&resources).Error; err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+	var users []User
+	if err := db.Find(&users).Error; err != nil {
+	w.WriteHeader(http.StatusInternalServerError)
+	return
 	}
 
-	if err := json.NewEncoder(w).Encode(&resources); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+
 }
 
 func GetResource(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var resource Product
-	db.First(&resource, params["key"])
-	if err := json.NewEncoder(w).Encode(&resource); err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(err.Error()))
-		return
-	}
-	json.NewEncoder(w).Encode(&resource)
-}
-
-func CreateResource(w http.ResponseWriter, r *http.Request) {
-	var resource Product
+	var resource User
 	if err := json.NewDecoder(r.Body).Decode(&resource); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(err.Error()))
 		return
 	}
-
-	if err := db.Create(&resource).Error; err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+	/*params := mux.Vars(r)
+	db.First(&resource, params["card_key"])
+	i,_:=strconv.ParseInt(params["card_key"], 10, 64)
+	if err := db.Where("card_key = ?", i).First(&resource).Error; err != nil {
+		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte(err.Error()))
 		return
-	}
-
+	}*/
 	json.NewEncoder(w).Encode(&resource)
-}
-
-func DeleteResource(w http.ResponseWriter, r *http.Request) {
-
-	var resource Product
-	db.First(&resource)
-	db.Delete(&resource)
-
-	var resources []Product
-	if err := db.Find(&resources).Error; err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if err := json.NewEncoder(w).Encode(&resources); err != nil {
-		w.WriteHeader(http.StatusNoContent)
-		w.Write([]byte(err.Error()))
-		return
-	}
 }
